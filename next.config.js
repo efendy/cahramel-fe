25f69e295962ae3555cacdf1adb3db16c4ac37e2
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-06 16:44:20 +0630

    chore: fix config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@
  const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
