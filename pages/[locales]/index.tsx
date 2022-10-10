654185832cc6662dc61e1ed600e1178cdcf5f377
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-10 21:50:29 +0800

    added locale detectors

---
+++ pages/[locales]/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push('/auth/login');
+    }
+    if (status === "authenticated") {
+      router.push('/app');
+    }
+  }, [router, status]);
+
+  return (
+    <></>
+  );
+};
+
+export default Home;
