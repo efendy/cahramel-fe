ac11cd14a227d8901fb775768bc4a818278b3940
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-10 15:36:29 +0800

    fix logo alignment

--- pages/auth/reset.tsx
+++ pages/auth/reset.tsx
@@ -9,7 +9,7 @@ const AppResetPage = () => {
     <>
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <Logo size={LogoSize.lg} />
+          <div className="mx-auto w-max"><Logo size={LogoSize.lg} /></div>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Reset Password</h2>
         </div>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
