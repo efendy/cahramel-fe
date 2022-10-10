c3f123f9a873ca99873d40153a01c17181b3055c
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-10 22:07:55 +0800

    fix locale navigations

--- pages/[locales]/index.tsx
+++ pages/[locales]/index.tsx
@@ -1,20 +1,22 @@
+import languageDetector from "@lib/language-detector";
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const detectedLng = languageDetector.detect();
   const router = useRouter();
   const { status } = useSession();
 
   useEffect(() => {
     if (status === "unauthenticated") {
-      router.push('/auth/login');
+      router.push(`/${detectedLng}/auth/login`);
     }
     if (status === "authenticated") {
-      router.push('/app');
+      router.push(`/${detectedLng}/app`);
     }
-  }, [router, status]);
+  }, [router, status, detectedLng]);
 
   return (
     <></>
