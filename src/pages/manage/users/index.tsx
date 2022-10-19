172ada0a75c3000efc1dab81751af31f59d6915f
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-19 10:16:48 +0800

    feat: added ui for draft user

--- src/pages/manage/users/index.tsx
+++ src/pages/manage/users/index.tsx
@@ -144,6 +144,262 @@ const ManageUsersPage = () => {
         <Pagination />
       </ClientLayout>
       <SlidePanel open={open} setOpen={setOpen} title={"Draft User"} subtitle={"Every moment is a fresh beginning."}>
+        <form className="pb-8 space-y-6" action="#" method="POST">
+          <div className="px-4 py-5 sm:p-6">
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              <div className="col-span-full">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+                <p className="mt-1 text-sm text-gray-500">Use a permanent address where you can receive mail.</p>
+              </div>
+              <div className="mt-5 md:col-span-2 md:mt-0">
+                <div className="grid grid-cols-6 gap-6">
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-4">
+                    <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      name="email-address"
+                      id="email-address"
+                      autoComplete="email"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-4">
+                    <label htmlFor="phone-no" className="block text-sm font-medium text-gray-700">
+                      Phone number (optional)
+                    </label>
+                    <input
+                      type="text"
+                      name="phone-no"
+                      id="phone-no"
+                      autoComplete="phone"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-200 h-[2px] w-full" />
+          <div className="px-4 py-5 sm:p-6">
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              <div className="col-span-full">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">Contract Information</h3>
+                <p className="mt-1 text-sm text-gray-500">Decide which communications you'd like to receive and how.</p>
+              </div>
+              <div className="mt-5 md:col-span-3 md:mt-0 ">
+                <div className="grid grid-cols-6 gap-6">
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="employee-id" className="block text-sm font-medium text-gray-700">
+                      Employee ID
+                    </label>
+                    <input
+                      type="text"
+                      name="employee-id"
+                      id="employee-id"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="job-title" className="block text-sm font-medium text-gray-700">
+                      Job title
+                    </label>
+                    <select
+                      id="job-title"
+                      name="job-title"
+                      className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option>FullStack Developer</option>
+                      <option>Software Developer</option>
+                      <option>Mobile Developer</option>
+                    </select>
+                  </div>
+
+                  <div className="col-span-6">
+                    <label htmlFor="department" className="block text-sm font-medium text-gray-700">
+                      Department (optional)
+                    </label>
+                    <input
+                      type="text"
+                      name="department"
+                      id="department"
+                      autoComplete="department"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="reporting-main" className="block text-sm font-medium text-gray-700">
+                      Reporting to main
+                    </label>
+                    <select
+                      id="reporting-main"
+                      name="reporting-main"
+                      className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option>John</option>
+                      <option>Steve</option>
+                    </select>
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="reporting-secondary" className="block text-sm font-medium text-gray-700">
+                      Reporting to secondary (optional)
+                    </label>
+                    <select
+                      id="reporting-secondary"
+                      name="reporting-secondary"
+                      className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option>Steve</option>
+                      <option>John</option>
+                    </select>
+                  </div>
+
+                  <div className="col-span-6">
+                    <label htmlFor="start-date" className="block text-sm font-medium text-gray-700">
+                      Commencement date
+                    </label>
+                    <input
+                      type="date"
+                      name="start-date"
+                      id="start-date"
+                      autoComplete="start-date"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                  <div className="col-span-full relative flex items-start">
+                    <div className="flex h-5 items-center">
+                      <input
+                        id="onboard"
+                        aria-describedby="onboard"
+                        name="onboard"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label htmlFor="onboard" className="font-medium text-gray-700">
+                        Need Onboarding?
+                      </label>
+                      <p id="onboard" className="text-gray-500">
+                        Get notified when checked.
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="col-span-6">
+                    <label htmlFor="end-date" className="block text-sm font-medium text-gray-700">
+                      Termination date
+                    </label>
+                    <input
+                      type="date"
+                      name="end-date"
+                      id="end-date"
+                      autoComplete="end-date"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                  <div className="col-span-full relative flex items-start">
+                    <div className="flex h-5 items-center">
+                      <input
+                        id="offboard"
+                        aria-describedby="offboard"
+                        name="offboard"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label htmlFor="offboard" className="font-medium text-gray-700">
+                        Need Offboarding?
+                      </label>
+                      <p id="offboard" className="text-gray-500">
+                        Get notified when checked.
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-4">
+                    <label htmlFor="corp-email" className="block text-sm font-medium text-gray-700">
+                      Corporate email address
+                    </label>
+                    <input
+                      type="text"
+                      name="corp-email"
+                      id="corp-email"
+                      autoComplete="email"
+                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-3">
+                    <label htmlFor="access-role" className="block text-sm font-medium text-gray-700">
+                      Access Role
+                    </label>
+                    <select
+                      id="access-role"
+                      name="access-role"
+                      className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                      <option>User</option>
+                      <option>Admin</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-end pr-8">
+            <button
+              type="button"
+              className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="ml-3 inline-flex justify-center rounded-md border border-indigo-600 py-2 px-4 text-sm font-medium text-indigo-700 shadow-sm hover:text-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Save as draft
+            </button>
+            <button
+              type="submit"
+              className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Confirm and send out invitation
+            </button>
+          </div>
+        </form>
       </SlidePanel>
     </>
   )
