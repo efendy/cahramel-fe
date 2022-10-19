32b39646d67d7a0a1874c02b39337175ba5ebccf
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-19 10:15:53 +0800

    fix: added provider for query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../assets/styles/globals.css'
+import { useState } from 'react';
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export default function App({ Component, pageProps: {session, ...pageProps } }: AppProps) {
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
