9e456386ed41463e2482a85e82b79dc34306de6e
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-19 17:28:48 +0800

    fix: fix jwt

--- src/pages/api/user/profile.ts
+++ src/pages/api/user/profile.ts
@@ -7,27 +7,22 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'GET' || token == null) {
         return res.status(405).json({ error: { message: 'Invalid request' } })
     }
-    // console.log('tttt', token)
     try {
         const user = await client('user-profiles/me', 'GET', {
-            token: token.jwt as string
+            token: token.accessToken as string
         })
-        console.log('uereere', token)
         const contract = await client(
             `user-contracts?filters[user_profile]=${user.id}`,
             'GET',
             {
-                token: token.jwt as string
+                token: token.accessToken as string
             }
         )
-        console.log('here 10')
         let userContract = null
         if (contract?.data?.length > 0) {
             const cData = contract.data[0]
             userContract = { ...cData?.attributes, id: cData.id }
         }
-        console.log('here 20', { ...user.attributes, id: user.id, contract: userContract })
-        // console.log({ ...user.attributes, id: user.id, contract: userContract })
         return res
             .status(200)
             .json({ ...user.attributes, id: user.id, contract: userContract })
