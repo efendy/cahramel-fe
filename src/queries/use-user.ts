55af11143a9111983c67b1591083cb7ed84e854f
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-28 11:14:05 +0800

    fix: update use user query

--- src/queries/use-user.ts
+++ src/queries/use-user.ts
@@ -2,14 +2,24 @@ import {UserProfileType} from '@interfaces/user-profile';
 import {useQuery} from '@tanstack/react-query';
 import {queryClient} from '@utils/api-client';
 
-export const useGetProfile = () => {
-  const result = useQuery(['user'], async () => await getUser(), {
-    keepPreviousData: true,
+export const useGetProfile = ({
+  enabled,
+  onSuccess,
+}: {
+  enabled?: boolean;
+  onSuccess?: (data: ProfileResponseType) => void;
+}) => {
+  return useQuery(['user-profile'], async () => await getUserProfile(), {
+    enabled,
+    onSuccess,
   });
-  return result;
 };
 
-const getUser = async () => {
+export type ProfileResponseType =
+  | Awaited<ReturnType<typeof getUserProfile>>
+  | undefined;
+
+const getUserProfile = async () => {
   return queryClient(
     'user-profiles/me?populate[]=user_contracts.company_profile&populate[]=user_contracts.company_profile.image_profile&populate[]=user_contracts.company_profile.industry_type&populate[]=image_profile',
     'GET',
@@ -17,27 +27,16 @@ const getUser = async () => {
   ).then(data => {
     const queryData = data as UserProfileType;
     const attributes = queryData.attributes;
-    let user_contract = null;
-    if (
-      attributes?.user_contracts?.data &&
-      attributes?.user_contracts?.data?.length > 0
-    ) {
-      const contract = attributes?.user_contracts?.data[0];
-      const contractA = contract.attributes;
-      const companyProfile = {
-        ...contractA?.company_profile?.data?.attributes,
-        id: contractA?.company_profile?.data?.id,
-      };
-
-      user_contract = {
-        ...contract.attributes,
-        id: contract.id,
-        company_profile: companyProfile,
-      };
-    }
+    // let user_contracts = null;
+    const contracts = attributes?.user_contracts?.data;
     return {
       ...attributes,
-      user_contract,
+      user_contracts: contracts
+        ? contracts.map(x => ({
+            ...x.attributes,
+            id: x.id,
+          }))
+        : undefined,
       id: queryData.id,
     };
   });
