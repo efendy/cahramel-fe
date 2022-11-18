73a011bbc75ac17f7399e14d71a95e1fb0541a65
junhao-h <44504072+junhao-h@users.noreply.github.com>
2022-11-18 11:12:00 +0800

    FixUi

---
+++ src/components/forms/edit-profile.tsx
@@ -0,0 +1,268 @@
+import Image from 'next/image';
+import {useForm} from 'react-hook-form';
+
+type FormValues = {
+  first_name: string;
+  last_name: string;
+  gender: string;
+  marital_status: number;
+  birthday: Date;
+  email: string;
+  unique_id: string;
+  phone: number;
+  nationality: string;
+  race: string;
+  religion: string;
+  image_blobs: Blob[];
+};
+
+export const ProfileForm = ({isCreate}: {isCreate?: boolean}) => {
+  const {register} = useForm<FormValues>();
+
+  if (!isCreate) {
+    //return null;
+  }
+  return (
+    <form className="divide-y-amber-gray-200 mt-6 mb-12 space-y-8 divide-y">
+      <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-6 sm:gap-x-6">
+        <div className="sm:col-span-6">
+          <label
+            htmlFor="photo"
+            className="block text-sm font-medium text-gray-700">
+            Photo
+          </label>
+          <div className="mt-1 flex items-center">
+            <Image
+              className="inline-block rounded-full"
+              src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80"
+              alt=""
+              width={48}
+              height={48}
+            />
+            <div className="ml-4 flex">
+              <div className="relative flex cursor-pointer items-center rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus-within:outline-none focus-within:ring-2 focus-within:ring-amber-500 focus-within:ring-offset-2 focus-within:ring-offset-amber-gray-50 hover:bg-amber-gray-50">
+                <label
+                  htmlFor="user-photo"
+                  className="pointer-events-none relative text-sm font-medium text-gray-700">
+                  <span>Change</span>
+                  <span className="sr-only"> user photo</span>
+                </label>
+                <input
+                  id="user-photo"
+                  name="user-photo"
+                  type="file"
+                  className="absolute inset-0 h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0"
+                />
+              </div>
+              <button
+                type="button"
+                className="ml-3 rounded-md border border-transparent bg-transparent py-2 px-3 text-sm font-medium focus:border-gray-300 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2 focus:ring-offset-amber-gray-50 text-gray-700">
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="first-name"
+            className="block text-sm font-medium text-gray-700">
+            First name
+          </label>
+          <input
+            {...register('first_name')}
+            required
+            type="text"
+            name="first-name"
+            id="first-name"
+            autoComplete="given-name"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="last-name"
+            className="block text-sm font-medium text-gray-700">
+            Last name
+          </label>
+          <input
+            type="text"
+            name="last-name"
+            id="last-name"
+            autoComplete="family-name"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="gender"
+            className="block text-sm font-medium text-gray-700">
+            Gender
+          </label>
+          <select
+            id="gender"
+            name="gender"
+            autoComplete="gender-name"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm">
+            <option />
+            <option>Male</option>
+            <option>Female</option>
+            <option>Non-binary</option>
+          </select>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="marital_status"
+            className="block text-sm font-medium text-gray-700">
+            Marital Status
+          </label>
+          <select
+            id="marital_status"
+            name="marital_status"
+            autoComplete="marital-status"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm">
+            <option />
+            <option>Single</option>
+            <option>Married</option>
+            <option>Widowed</option>
+            <option>Separated</option>
+            <option>Divorced</option>
+            <option>Not Reported</option>
+          </select>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="birthday"
+            className="block text-sm font-medium text-gray-700">
+            Birthday
+          </label>
+          <input
+            type="date"
+            name="birthday"
+            id="birthday"
+            autoComplete="birthday"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 gap-y-6 pt-8 sm:grid-cols-6 sm:gap-x-6">
+        <div className="sm:col-span-6">
+          <h2 className="text-xl font-medium">Personal Information</h2>
+          <p className="mt-1 text-sm">
+            This information will be displayed publicly so be careful what you
+            share.
+          </p>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="email-address"
+            className="block text-sm font-medium text-gray-700">
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email-address"
+            id="email-address"
+            autoComplete="email"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="phone-number"
+            className="block text-sm font-medium text-gray-700">
+            Phone number
+          </label>
+          <input
+            type="tel"
+            name="phone-number"
+            id="phone-number"
+            autoComplete="tel"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="nationality"
+            className="block text-sm font-medium text-gray-700">
+            Nationality
+          </label>
+          <select
+            id="nationality"
+            name="nationality"
+            autoComplete="nationality-name"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm">
+            <option />
+            <option>United States</option>
+            <option>Singapore</option>
+            <option>Malaysia</option>
+            <option>Vietnam</option>
+            <option>Myanmmar</option>
+          </select>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="race"
+            className="block text-sm font-medium text-gray-700">
+            Race
+          </label>
+          <input
+            type="text"
+            name="race"
+            id="race"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="religion"
+            className="block text-sm font-medium text-gray-700">
+            Religion
+          </label>
+          <select
+            id="religion"
+            name="religion"
+            autoComplete="-religion"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-amber-500 focus:ring-amber-500 sm:text-sm">
+            <option />
+            <option>Buddhist</option>
+            <option>Christianity</option>
+            <option>Hinduism</option>
+            <option>Islam</option>
+            <option>Free thinker</option>
+          </select>
+        </div>
+
+        <p className="text-sm sm:col-span-6">
+          This account was created on{' '}
+          <time dateTime="2017-01-05T20:35:40">
+            January 5, 2017, 8:35:40 PM
+          </time>
+          .
+        </p>
+      </div>
+
+      <div className="flex justify-end pt-8">
+        <button
+          type="button"
+          className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium shadow-sm hover:bg-amber-gray-50 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-amber-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-amber-500 focus:ring-offset-2">
+          Save
+        </button>
+      </div>
+    </form>
+  );
+};
